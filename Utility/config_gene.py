bdfed918b58497ceb32887f90a086680e1eabf8a
Kirby Zhou <kirbyzhou@protonmail.com>
2020-03-22 18:32:09 -0700

    Add edge menu json config generator script

--- Utility/config_gene.py
+++ Utility/config_gene.py
@@ -2,13 +2,15 @@ import json
 
 # in order to be very generic, everything should be parsed recursively.
 configFile = {}
+defaultVisibility = True
 individualPropertyTemplate = {
     'isLeaf': True,
     'type': '',
     'property name': '',
     'default value': None,
     'options': {},
-    'metadata': None
+    'metadata': None,
+    'visibility': defaultVisibility
 }
 
 dropDownMetadata = {}
@@ -17,6 +19,9 @@ menuType = ['Node', 'Edge']
 
 subNodeType = ['Global Scale Setting', 'Node Channels', 'Node Filter', 'Node Lable Setting']
 subEdgeType = ['Global Scale setting', 'Edge Channels', 'Edge Filter', 'Edge Lable Setting']
+
+# Node menu
+individualNodeProperties = {}
 
 individualNodeProperties['Global Scale Setting'] = {}
 individualNodeProperties['Global Scale Setting']['Node Size'] = individualPropertyTemplate
@@ -110,41 +115,118 @@ individualNodeProperties['Node Channels']['Node Border-Color']['Color Map']['def
 individualNodeProperties['Node Channels']['Node Border-Color']['Color Map']['options'] = dropDownMetadata
 
 individualNodeProperties['Node Filter'] = {}
+individualNodeProperties['Node Filter']['type'] = 'filter'
 
 individualNodeProperties['Node Lable Setting'] = {}
+individualNodeProperties['Node Lable Setting']['properties selection'] = individualPropertyTemplate
+individualNodeProperties['Node Lable Setting']['properties selection']['name'] = 'Select property shown on label'
+individualNodeProperties['Node Lable Setting']['properties selection']['type'] = 'overall_selection'
 
-defaultVisibility = True
+individualNodeProperties['Node Lable Setting']['properties display'] = individualPropertyTemplate
+individualNodeProperties['Node Lable Setting']['properties display']['name'] = 'Click Node to see Label'
+individualNodeProperties['Node Lable Setting']['properties display']['type'] = 'associate_display'
+individualNodeProperties['Node Lable Setting']['properties display']['metadata'] = 'properties selection'
 
-subProto = {
-    'attr': 'Categorical',
-    'visibility': defaultVisibility,
-    'options': {},
-    'default_value': None
+# Edge menu
+individualEdgeProperties = {}
+
+individualEdgeProperties['Global Scale Setting'] = {}
+individualEdgeProperties['Global Scale Setting']['Edge Width'] = individualPropertyTemplate
+individualEdgeProperties['Global Scale Setting']['Edge Width']['type'] = 'fill_in'
+individualEdgeProperties['Global Scale Setting']['Edge Width']['property name']= 'Edge Width'
+individualEdgeProperties['Global Scale Setting']['Edge Width']['default value'] = 2
+
+individualEdgeProperties['Global Scale Setting']['Edge Color'] = individualPropertyTemplate
+individualEdgeProperties['Global Scale Setting']['Edge Color']['type'] = 'fill_in'
+individualEdgeProperties['Global Scale Setting']['Edge Color']['property name']= 'Edge Color'
+individualEdgeProperties['Global Scale Setting']['Edge Color']['default value'] = 'FF0000'
+
+individualEdgeProperties['Global Scale Setting']['Edge Opacity'] = individualPropertyTemplate
+individualEdgeProperties['Global Scale Setting']['Edge Opacity']['type'] = 'fill_in'
+individualEdgeProperties['Global Scale Setting']['Edge Opacity']['property name']= 'Edge Opacity'
+individualEdgeProperties['Global Scale Setting']['Edge Opacity']['default value'] = 100
+
+dropDownMetadata = {}
+dropDownMetadata['Full Line'] = '/static/src/rcs/full-line.png'
+dropDownMetadata['Dash Line'] = '/static/src/rcs/dash-line.png'
+dropDownMetadata['Arrow Line'] = '/static/src/rcs/arrow-line.png'
+dropDownMetadata['Arror Dash Line'] = '/static/src/rcs/arrow-dash-line.png'
+
+individualEdgeProperties['Global Scale Setting']['Edge Shape'] = individualPropertyTemplate
+individualEdgeProperties['Global Scale Setting']['Edge Shape']['type'] = 'static_selection'
+individualEdgeProperties['Global Scale Setting']['Edge Shape']['property name']= 'Edge Shape'
+individualEdgeProperties['Global Scale Setting']['Edge Shape']['default value'] = 'default'
+individualEdgeProperties['Global Scale Setting']['Edge Shape']['options'] = dropDownMetadata
+
+individualEdgeProperties['Edge Channels'] = {}
+individualEdgeProperties['Edge Channels']['Edge Width'] = {}
+individualEdgeProperties['Edge Channels']['Edge Width']['Edge Property'] = individualPropertyTemplate
+individualEdgeProperties['Edge Channels']['Edge Width']['Edge Property']['type'] = 'populate_selection'
+individualEdgeProperties['Edge Channels']['Edge Width']['Edge Property']['property name']= 'Edge Property'
+individualEdgeProperties['Edge Channels']['Edge Width']['Edge Property']['default value'] = 'default'
+
+individualEdgeProperties['Edge Channels']['Edge Color'] = {}
+individualEdgeProperties['Edge Channels']['Edge Color']['Edge Property'] = individualPropertyTemplate
+individualEdgeProperties['Edge Channels']['Edge Color']['Edge Property']['type'] = 'populate_selection'
+individualEdgeProperties['Edge Channels']['Edge Color']['Edge Property']['property name']= 'Edge Property'
+individualEdgeProperties['Edge Channels']['Edge Color']['Edge Property']['default value'] = 'default'
+
+dropDownMetadata = {}
+dropDownMetadata['Categorical'] = {
+    'Tableau10': '/static/src/rcs/Tableau10.png',
+    'Set3': '/static/src/rcs/Set3.png',
+    'Pastel1': '/static/src/rcs/Pastel1.png',
+    'Paired': '/static/src/rcs/Paired.png',
+    'Dark2': '/static/src/rcs/Dark2.png',
+    'Category10': '/static/src/rcs/Category10.png',
+    'Accent': '/static/src/rcs/Accent.png'
 }
+dropDownMetadata['Sequential (Single Hue)'] = {
+    'Blues': '/static/src/rcs/Blues[k].png',
+    'Greys': '/static/src/rcs/Greys[k].png',
+    'Greens': '/static/src/rcs/Greens[k].png',
+    'Oranges': '/static/src/rcs/Oranges[k].png',
+    'Purples': '/static/src/rcs/Purples[k].png',
+    'Reds': '/static/src/rcs/Reds[k].png'
+}
+dropDownMetadata['Sequential (Multi-Hue)'] = {
+    'Sinebow': '/static/src/rcs/Sinebow(t).png',
+    'Spectral': '/static/src/rcs/Spectral[k].png'
+}
+
+individualEdgeProperties['Edge Channels']['Edge Color']['Color Map'] = individualPropertyTemplate
+individualEdgeProperties['Edge Channels']['Edge Color']['Color Map']['type'] = 'static_selection'
+individualEdgeProperties['Edge Channels']['Edge Color']['Color Map']['property name']= 'Color Map'
+individualEdgeProperties['Edge Channels']['Edge Color']['Color Map']['default value'] = 'default'
+individualEdgeProperties['Edge Channels']['Edge Color']['Color Map']['options'] = dropDownMetadata
+
+individualEdgeProperties['Edge Channels']['Edge Opacity'] = {}
+individualEdgeProperties['Edge Channels']['Edge Opacity']['Edge Property'] = individualPropertyTemplate
+individualEdgeProperties['Edge Channels']['Edge Opacity']['Edge Property']['type'] = 'populate_selection'
+individualEdgeProperties['Edge Channels']['Edge Opacity']['Edge Property']['property name']= 'property name'
+individualEdgeProperties['Edge Channels']['Edge Opacity']['Edge Property']['default value'] = 'default'
+
+individualEdgeProperties['Edge Filter'] = {}
+individualEdgeProperties['Edge Filter']['type'] = 'filter'
+
+individualEdgeProperties['Edge Lable Setting'] = {}
+individualEdgeProperties['Edge Lable Setting']['properties selection'] = individualPropertyTemplate
+individualEdgeProperties['Edge Lable Setting']['properties selection']['name'] = 'Select property shown on label'
+individualEdgeProperties['Edge Lable Setting']['properties selection']['type'] = 'overall_selection'
+
+individualEdgeProperties['Edge Lable Setting']['properties display'] = individualPropertyTemplate
+individualEdgeProperties['Edge Lable Setting']['properties display']['name'] = 'Click Edge to see Label'
+individualEdgeProperties['Edge Lable Setting']['properties display']['type'] = 'associate_display'
+individualEdgeProperties['Edge Lable Setting']['properties display']['metadata'] = 'properties selection'
 
 metadata = {
     'Node': subNodeType,
     'Edge': subEdgeType
 }
 
-#  'Graph': ['Network', 'Clicked Node', 'Clicked Edge'],
-#  'Layout': ['By Prominence Index', 'By Force-Directed Mondel']
-
-cateList = ['color', 'shape']
-contList = ['size', 'alpha', 'border_size']
-
-for mtype in menuType:
-    configFile[mtype] = {}
-    for ele in cateList:
-        configFile[mtype][ele] = subProto
-
-subProto['attr'] = 'Continuous'
-for mtype in menuType:
-    configFile[mtype] = {}
-    for ele in contList:
-        configFile[mtype][ele] = subProto
-
 configFile['metadata'] = metadata
+configFile['Node'] = individualNodeProperties
+configFile['Edge'] = individualEdgeProperties
 
 with open('../static/config_skeleton.json', 'w') as outfile:
     json.dump(configFile, outfile, indent=2)
