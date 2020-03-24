18efea3f0e56d75155f62972dd8f33ac384a84cc
Kirby Zhou <kirbyzhou@protonmail.com>
2020-03-24 12:15:02 -0700

    Add skeleton for menu_populator

--- static/src/js/menu_populator.js
+++ static/src/js/menu_populator.js
@@ -1,8 +1,22 @@
 var menuConfig;
+var levelStyle = {
+  0: "",
+  1: "",
+  2: "",
+  3: ""
+}
 
 function populate_menu() {
   $.get("/menu", function (data) {
     menuConfig = data;
-    console.log(menuConfig);
+    // console.log(menuConfig);
+
   })
+}
+
+// key: current label needs to be populated
+// level: current label level
+// config: json configration object
+function recusive_parse(key, level, config) {
+
 }
